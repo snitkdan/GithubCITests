00a925428767b493be8902c2a91704a8d85f9bd5
Daniel Snitkovskiy <snitkdan@uw.edu>
2019-11-30 21:11:34 -0800

    make breaking test

--- tests.py
+++ tests.py
@@ -4,6 +4,9 @@ class DummyTest(unittest.TestCase):
 
     def dummy_test(self):
         self.assertEqual(1, 1)
+    
+    def dummy_test2(self):
+        self.assertEqual(1, 2)
 
 if __name__ == '__main__':
     unittest.main()
